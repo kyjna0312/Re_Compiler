909b4a08678c3d8844a866c440e234ecf6bd0718
youngjin <kyjna0312@gmail.com>
2021-04-09 16:03:48 +0900

    clear get_token(base)

--- hw1.c
+++ hw1.c
@@ -1,10 +1,51 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+void get_token();
+int expreesion();
+int term();
+int factor();
+void error(int i);
 
 int num;
 enum {Null, NUMBER, PLUS, STAR, LPAREN, RPAREN, END} token;
 
 void get_token(){
+    char ch = getchar();
 
+    if(isdigit(ch)){
+        int number = 0;
+        token = NUMBER;
+        num = number * 10 + ch - '0';
+        printf("number : %d\n", num);
+    }
+    else{
+        switch (ch){
+            case '+': 
+                    token = PLUS;
+                    printf("plus\n");
+                    break;
+            case '*': 
+                    token = STAR;
+                    printf("star\n");
+                    break;
+            case '(':
+                    token = LPAREN;
+                    printf("LPAREN\n");
+                    break;
+            case ')':
+                    token = RPAREN;
+                    printf("RPAREN\n");
+                    break;
+            case '\n':
+                    token = END;
+                    printf("end\n");
+                    break;
+            default :
+                    error(4);
+        }
+    }
 }
 
 int expression(){
@@ -21,10 +62,11 @@ int expression(){
 
 int term(){
     int result;
+    result = factor();
 
     if(token == STAR){
         get_token();
-        result = result*factor();
+        result = result * factor();
     }
 
     return(result);
@@ -72,6 +114,7 @@ void error(int i){
         case 1 : printf("error: number or '(' expected \n"); break;
         case 2 : printf("error: ')' expected \n"); break;
         case 3 : printf("error: EOF expected \n"); break;
+        case 4 : printf("error: Not definition charactor\n"); break;
     }
     exit(1);
 }
